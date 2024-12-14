fbcf5d2b1fef29623a9a208ac57760d91c2e9099
nathansivalingam <nathansivalingampersonal@gmail.com>
2024-12-14 19:46:41 +1100

    changed the logo - what did i even do today

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,7 +19,7 @@ const HomePage = function() {
         </SocialMediaStyle>
         <StyledImg src={aboutMeImg} alt="aboutMeImg"></StyledImg>
         <AboutMeStyle><b>About Me</b></AboutMeStyle>
-        <AboutMeParagraphStyle>This website is a project I’ve developed to improve my frontend programming skills. It serves as an interactive web-based version of my resume, and is linked to my LinkedIn profile to better engage with potential recruiters. Additionally, this project showcases my frontend development skills and will be a valuable asset as I pursue software engineering and computer science roles in the industry. The projects showcased on this website also include some of Mechanical Engineering endeavours which will be advantageous during applications for Mechanical Engineering internships.</AboutMeParagraphStyle>
+        <AboutMeParagraphStyle>Hi, my name is Nathan. I’m a penultimate-year student at UNSW pursuing a double degree in Mechanical Engineering and Computer Science. With a passion for integrating technology and engineering, I plan to address challenges in fields like robotics and automation. I'm excited to connect with like-minded professionals and explore innovative opportunities!</AboutMeParagraphStyle>
       </HomePageStyle>
     </div>
   </>
